d484d07ff229beb7e06d5b969be98cbf0544bae2
Aman <amankumar105541@gmail.com>
2025-12-06 15:20:57 +0530

    Typescript setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hello ${name} from typescript`;
+}
+const username = "aman";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(name :string):string{
+    return `hello ${name} from typescript`;
+}
+const username:string="aman";
+console.log(greet(username));
